12ee3db5446bb77468bc69e217153dfd66e7bc83
Tim <timothy.hjellum@eia.gov>
2018-05-22 16:02:56 -0400

    accordions

--- analysis/index.php
+++ analysis/index.php
@@ -7,20 +7,20 @@
 ?>
 <html>
 <head>
-<?php include('css_rehab/archive/global/head/includes/head.inc') ?>
+<?php include('adaptive/global/head/includes/head.inc') ?>
 <?php  if(!isset($no_titling)) include('global/includes/titling.inc') ?>
 </head>
 <body>
-<?php include('css_rehab/archive/global/header/includes/header.inc') ?>
+<?php include('adaptive/global/header/includes/header.inc') ?>
 <?php include('./includes/sub-navigation.inc'); ?>
-<?php // include('css_rehab/archive/global/sliders/multi-card-slider.inc') // ?>
+<?php // include('adaptive/global/sliders/multi-card-slider.inc') // ?>
 <div class="l-row">
 	<div class="l-two-col">
 		<?php include('./includes/recent-data.inc') ?>
 		<?php include('./includes/updated-data-series.inc') ?>
 	</div>
 	<div class="l-two-col">
-		<?php include('css_rehab/archive/global/analysis-projections/analysis-projections.inc') ?>
+		<?php include('adaptive/global/analysis-projections/analysis-projections.inc') ?>
 		<?php include('./includes/presentations.inc') ?>
 	</div>
 </div>
--- coal/data.php
+++ coal/data.php
@@ -5,21 +5,21 @@ $locale = 'data';
 ?>
 <html>
 <head>
-<?php include('css_rehab/archive/global/head/includes/head.inc') ?>
+<?php include('adaptive/global/head/includes/head.inc') ?>
 <?php  if(!isset($no_titling)) include('global/includes/titling.inc') ?>
 </head>
 <body>
-<?php include('css_rehab/archive/global/header/includes/header.inc') ?>
+<?php include('adaptive/global/header/includes/header.inc') ?>
 <?php include('./includes/sub-navigation.inc'); ?>
 <div class="l-row">
 	<div class="l-main-col">
-		<?php include('includes/main-data.inc'); ?>
+		<?php include('includes/main-data.html'); ?>
 		<?php include('includes/weeklies.inc'); ?>
 	</div>
 	<div class="l-narrow-col">
 		<?php include('includes/side-data.inc'); ?>
 	</div>
 </div>
-<?php include('css_rehab/archive/global/footer/includes/footer.inc') ?>
+<?php include('adaptive/global/footer/includes/footer.inc') ?>
 </body>
 </html>
